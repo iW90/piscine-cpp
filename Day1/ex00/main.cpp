b202041cd6eb356eacc48797e079af73c0f6534f
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-16 22:31:29 -0300

    feat: implement functions ex00

--- Day1/ex00/main.cpp
+++ Day1/ex00/main.cpp
@@ -3,15 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 11:37:05 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/16 15:01:44 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/16 22:30:43 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
+int main()
+{
+	Account accountA = Account(0, 100);
+	Account accountB = Account(1, 100);
+
+	Bank bank = Bank();
+	bank.createAccount(0, 100);
+	bank.createAccount(1, 100);
+
+	bank.liquidity -= 200;
+	bank.modifyAccount(0, 500);
+
+	std::cout << "Account A: " << bank[0] << std::endl;
+	std::cout << "Account B: " << bank[1] << std::endl;
+
+	std::cout << " ----- " << std::endl;
+
+	std::cout << bank << std::endl;
+
+	return 0;
+}
+
+/*
 int main()
 {
 	Account accountA = Account();
@@ -41,3 +64,4 @@ int main()
 
 	return (0);
 }
+*/

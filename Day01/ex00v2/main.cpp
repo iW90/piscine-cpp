91e5a2ba922d4913626b8a6ddb4f457670e1ae1b
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-20 09:29:58 -0300

    feat: update main tests

--- Day01/ex00v2/main.cpp
+++ Day01/ex00v2/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 19:12:58 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/20 09:16:33 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/20 09:29:24 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int main()
 	Statistic stat = {0, 0};
 	std::vector<Tool*> tls;
 	Worker worker1(pos, stat, tls);
+	Worker worker2(pos, stat, tls);
 	
 	// Usando a ferramenta
 	shovel.use();
@@ -38,6 +39,16 @@ int main()
 	// Acessando o número de usos do martelo
 	int hammerUses = hammer.getNumberOfUses();
 	std::cout << "Número de usos do martelo: " << hammerUses << std::endl;
+
+	worker2.addTool(&shovel);
+	shovel.use();
+
+	// Acessando o número de usos da pá
+	shovelUses = shovel.getNumberOfUses();
+	std::cout << "Número de usos da pá: " << shovelUses << std::endl;
+
+
+
 
 	
 	//Shovel* shovelTool = worker1.getToolOfType<Shovel>();

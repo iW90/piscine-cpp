a27ce41a0c76f3e355563ecd07bddf430261662c
inwagner <inwagner@student.42sp.org.br>
2023-10-20 12:12:34 -0300

    feat: add remove worker from workshop

--- Day01/ex00v2/Workshop.hpp
+++ Day01/ex00v2/Workshop.hpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:35:17 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/20 12:11:18 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/20 12:12:23 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ class Workshop
 		Workshop();
 		~Workshop();
 
-		//getters
-		Worker*	getTool(Worker* worker) const;
-		void	setTool(Worker* worker);
+		Worker*	getWorker(Worker* worker) const;
+		void	setWorker(Worker* worker);
 
 		void	removeWorker(Worker* worker);
 		void	addWorker(Worker* worker, Tool* tool);

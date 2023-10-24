ad6514443a537789341c88acdf14f2d8f8475e2f
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-24 20:38:10 -0300

    fix: solve define error

--- Day03/ex00/Brakes.hpp
+++ Day03/ex00/Brakes.hpp
@@ -3,19 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   Brakes.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:58:37 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/24 16:51:41 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/24 20:37:22 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAKES_HPP
 # define BRAKES_HPP
 
-# define FORCE_MAX 200;
-
-#include <iostream>
+# define FORCE_MAX 200
 
 class Brakes
 {

29e90da67408a7cf2697540de432bcaeb7be3826
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-25 09:54:42 -0300

    feat: add new logic at ex03d3

--- Day03/ex03/main.cpp
+++ Day03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:48:42 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/25 09:52:20 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/25 09:54:35 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ int main()
 	manager.addEmployee(&contractEmployee);
 	manager.addEmployee(&apprentice);
 
-	// Set vacation for ContractEmployee and school hours for Apprentice
 	contractEmployee.setVacation(0);
 	apprentice.goSchool(0);
 	
@@ -38,39 +37,6 @@ int main()
 
 	return 0;
 }
-/*
-int main()
-{
-	EmployeeManager* manager = new EmployeeManager();
-
-	Employee* employee0 = new HourlyEmployee();
-	Employee* employee1 = new SalariedEmployee();
-	Employee* employee2 = new TempWorker(160); // 20 days * 8 hours/day
-	Employee* employee3 = new ContractEmployee(40); // 5 days * 8 hours/day
-	Employee* employee4 = new Apprentice(40, 80); // 5 days at school, 10 days at work
-
-	manager->addEmployee(employee0);
-	manager->addEmployee(employee1);
-	manager->addEmployee(employee2);
-	manager->addEmployee(employee3);
-	manager->addEmployee(employee4);
-
-	for (int i = 0; i < 30; i++)
-		manager->executeWorkday();
-
-	manager->calculatePayroll();
-
-	// Clean up memory
-	delete employee0;
-	delete employee1;
-	delete employee2;
-	delete employee3;
-	delete employee4;
-	delete manager;
-
-	return (0);
-}
-*/
 
 /*
 ** c++ -Wall -Wextra -Werror -std=c++98 -I . main.cpp && ./main

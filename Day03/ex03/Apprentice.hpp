325816852cc55e616cf37cd0d5f31c08452b9229
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-25 10:23:32 -0300

    feat: add new logic at ex03d3

--- Day03/ex03/Apprentice.hpp
+++ Day03/ex03/Apprentice.hpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:26:21 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/25 09:52:01 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/25 09:57:09 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ class Apprentice : public Employee
 {
 	private:
 		int schoolHours;
+		int workDay;
 
 	public:
-		Apprentice(int hourlyValue) : Employee(hourlyValue), schoolHours(0) {}
+		Apprentice(int hourlyValue) : Employee(hourlyValue), schoolHours(0), workDay(0) {}
 
 		int executeWorkday()
 		{
 			int hoursWorked = 7 - schoolHours / 2;
+			workDay += hourlyValue;
 			return hoursWorked;
 		}
 

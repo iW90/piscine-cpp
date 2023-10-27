2e2c40400fbdfd09329c967c76998afcd10ad917
inwagner <inwagner@student.42sp.org.br>
2023-10-27 11:41:13 -0300

    fix: resolve some bugs

--- Day04/ex00/Room.cpp
+++ Day04/ex00/Room.cpp
@@ -1,36 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Room.hpp                                           :+:      :+:    :+:   */
+/*   Room.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 16:38:05 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/27 11:40:41 by inwagner         ###   ########.fr       */
+/*   Created: 2023/10/27 11:40:19 by inwagner          #+#    #+#             */
+/*   Updated: 2023/10/27 11:40:52 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ROOM_HPP
-# define ROOM_HPP
+#include "Room.hpp"
 
-# include <vector>
-
-class Person;
-
-class Room
+void Room::exit(Person* p)
 {
-	private:
-		static int nextID;
-		int ID;
-		std::vector<Person*> _occupants;
-
-	public:
-		Room() : ID(++nextID) {};
-		~Room() {};
-		bool canEnter(Person* p);
-		void enter(Person* p) { _occupants.push_back(p); }
-		void exit(Person* p);
-		void printOccupant();
-};
-
-#endif
+	for (size_t i = 0; i < _occupants.size(); ++i)
+	{
+		if (_occupants[i] == p)
+		{
+			_occupants.erase(_occupants.begin() + i);
+			break;
+		}
+	}
+}

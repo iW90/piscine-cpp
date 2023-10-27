c0039111676d32d9bdd5ebebb457fefe0dede564
iW90 <ingrid.wagner.dev@gmail.com>
2023-10-27 01:11:11 -0300

    fix: update observer

--- Day04/ex00/Secretary.cpp
+++ Day04/ex00/Secretary.cpp
@@ -6,25 +6,25 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 22:55:08 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/26 23:01:01 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/27 01:10:53 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Secretary.hpp"
 
-std::unique_ptr<Form>	Secretary::createForm(FormType p_formType)
+std::unique_ptr<Form> Secretary::createForm(FormType p_formType, const std::string& arg1 = "", const std::string& arg2 = "", int arg3 = 0, int arg4 = 0)
 {
 	switch(p_formType)
 	{
 		case FormType::CourseFinished:
-			return std::make_unique<CourseFinishedForm>();
+			return std::make_unique <CourseFinishedForm>(arg1, arg2, arg3, arg4);
 		case FormType::NeedMoreClassRoom:
-			return std::make_unique<NeedMoreClassRoomForm>();
+			return std::make_unique <NeedMoreClassRoomForm>(arg1, arg3, arg4);
 		case FormType::NeedCourseCreation:
-			return std::make_unique<NeedCourseCreationForm>();
+			return std::make_unique <NeedCourseCreationForm>(arg1, arg3, arg4);
 		case FormType::SubscriptionToCourse:
-			return std::make_unique<SubscriptionToCourseForm>();
+			return std::make_unique <SubscriptionToCourseForm>(arg1, arg2);
 		default:
-			return NULL;
+			return nullptr;
 	}
 }
--- Day04/ex00/Secretary.hpp
+++ Day04/ex00/Secretary.hpp
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:44:43 by inwagner          #+#    #+#             */
-/*   Updated: 2023/10/27 00:34:14 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/10/27 01:11:03 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ class Secretary : public Staff
 	public:
 		Secretary(std::string p_name) : Staff(p_name) {}
 		~Secretary() {}
-		std::unique_ptr<Form>	createForm(FormType p_formType);
 		void archiveForm(std::unique_ptr<Form>) {};
+		std::unique_ptr<Form>	createForm(FormType p_formType, const std::string& arg1, const std::string& arg2, int arg3, int arg4);
 };
 
 #endif
